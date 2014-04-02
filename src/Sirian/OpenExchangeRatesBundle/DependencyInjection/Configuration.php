7094ab0c082e4a4d8d8fae8d4ce39ca568d16f3c
Vasily Khayrulin <sirianru@gmail.com>
2014-04-02 12:07:46 +0400

    Added DI

---
+++ src/Sirian/OpenExchangeRatesBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Sirian\OpenExchangeRatesBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $root = $treeBuilder->root('open_exchange_rates');
+
+        $root
+            ->children()
+                ->scalarNode('app_id')->isRequired()->end()
+                ->scalarNode('base_currency')->defaultValue('USD')->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
---
+++ src/Sirian/OpenExchangeRatesBundle/DependencyInjection/OpenExchangeRatesExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sirian\OpenExchangeRatesBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+class OpenExchangeRatesExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+
+        $definition = $container->getDefinition('open_exchange_rates');
+        $definition
+            ->addArgument($config['app_id'])
+            ->addArgument($config['base_currency'])
+        ;
+    }
+}
